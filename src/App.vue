176799d7db5913f1aab631cfba6c640c563e5859
覃佳雷 <qinjialei@ipaynow.cn>
2018-08-29 23:49:58 +0800

    checkbox

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
-    <v-button size='middle'
+    <!-- <v-button size='middle'
               icon="poweroff">button</v-button>
 
     <v-button size='middle'
               icon="zhu"
               :loading='loading'
               @click="test"
-              iconPosition='right'>button</v-button> -->
+              iconPosition='right'>button</v-button> 
 
     <v-switch v-model="demo"></v-switch>
     <div>22222</div>
-    <v-switch v-model="demo"></v-switch>
+    <v-switch v-model="demo"></v-switch> -->
+    <v-checkbox :myValue='demo'
+                ref='demo'></v-checkbox>
+
+    <button @click="test">test</button>
 
   </div>
 </template>
@@ -20,26 +24,27 @@
 import VButton from "./components/button/button.vue";
 import vIcon from "./components/icon";
 import vSwitch from "./components/switch";
+import VCheckbox from "./components/checkbox.vue";
 
 export default {
   name: "app",
   components: {
     VButton,
     vIcon,
-    vSwitch
+    vSwitch,
+    VCheckbox
   },
   methods: {
     test() {
-      alert(1);
+      setTimeout(() => {
+        this.$refs.demo.update([1, 2, 3]);
+      }, 2000);
     }
   },
-  mounted() {
-  },
+  mounted() {},
   data() {
     return {
-      demo: false,
-      demo2: true,
-      loading: true
+      demo: []
     };
   }
 };
---
+++ src/components/checkbox.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class='m-checkbox'>
+        <div v-for='(item) in arr'
+             :key="item">
+            <input type="checkbox"
+                   :checked='binding[item]'> item
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    myValue: {
+      type: Array
+    }
+  },
+  data() {
+    return {
+      arr: [1, 2, 3],
+      binding: {}
+    };
+  },
+  methods: {
+    generate() {
+      let obj = {};
+      for (let i = 0; i < 10; i++) {
+        obj[i] = false;
+      }
+      this.binding = obj;
+    },
+    update(val) {
+      for (const item of val) {
+        this.binding[item] = true;
+      }
+      // alert(11)
+
+      //   for (const item of this.myValue) {
+      //     this.binding[item] = true;
+      //   }
+    }
+  },
+  mounted() {
+    this.generate();
+  },
+  components: {}
+};
+</script>
+
+<style lang='scss' scoped>
+.m-checkbox {
+}
+</style >
